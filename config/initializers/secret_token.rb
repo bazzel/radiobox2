75ccb4d54cbc2009211ac636333dc8e2fc092ae8
Patrick Baselier <patrick@kabisa.nl>
2013-09-16 19:48:43 +0200

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radiobox2::Application.config.secret_key_base = '3841b481679c559ab8377386d466874e19bdc31bdd259ab24f5c10343ca946c60a0cfaec53bf4e25479d43c8466621ecfe65a580e352d143d2c020603dd1a025'
