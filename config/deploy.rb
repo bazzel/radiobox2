186d86388919bc78d3a9f82b868211c41fcd568b
Patrick Baselier <patrick@kabisa.nl>
2013-09-19 19:17:44 +0200

    Config deploy.rb for passenger standalone

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # RVM bootstrap
 set :rvm_ruby_string, 'ruby-2.0.0-p247@radiobox2'
-require "rvm/capistrano"                               # Load RVM's capistrano plugin.
+require 'rvm/capistrano'                               # Load RVM's capistrano plugin.
 
 # When using whenever gem:
 # set :whenever_command, "bundle exec whenever"
@@ -12,40 +12,53 @@ set :rvm_type, :system
 require 'bundler/capistrano'
 
 # main details
-set :application, "radiobox2"
-role :web, "patrickbaselier.nl"
-role :app, "patrickbaselier.nl"
-role :db,  "patrickbaselier.nl", :primary => true
+set :rails_env,   'production'
+set :application, 'radiobox2'
+role :web,        'patrickbaselier.nl'
+role :app,        'patrickbaselier.nl'
+role :db,         'patrickbaselier.nl', :primary => true
 
 # server details
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-set :deploy_to, "/var/www/radiobox2"
-set :deploy_via, :remote_cache
-set :user, "passenger"
-set :use_sudo, false
+set :deploy_to,   '/var/www/radiobox2'
+set :deploy_via,  :remote_cache
+set :user,        'passenger'
+set :use_sudo,    false
 
 # repo details
 set :scm, :git
-set :scm_username, "passenger"
-set :repository, "git@github.com:bazzel/radiobox2.git"
-#set :branch, "master"
-set :branch, "feature-capistrano"
+set :scm_username,          'passenger'
+set :repository,            'git@github.com:bazzel/radiobox2.git'
+#set :branch, 'master'
+set :branch,                'feature-capistrano'
 set :git_enable_submodules, 1
+
+set :passenger_port,        3000
+set :passenger_cmd,         "#{bundle_cmd} exec passenger"
 
 # tasks
 namespace :deploy do
   task :start, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
+    #run "touch #{current_path}/tmp/restart.txt"
+    run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
   end
 
   task :stop, :roles => :app do
-    # Do nothing.
+    run "cd #{current_path} && #{passenger_cmd} stop -p #{passenger_port}"
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
+    #run "touch #{current_path}/tmp/restart.txt"
+    run <<-CMD
+      if [[ -f #{current_path}/tmp/pids/passenger.#{passenger_port}.pid ]];
+      then
+        cd #{current_path} && #{passenger_cmd} stop -p #{passenger_port};
+      fi
+    CMD
+
+    run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
   end
 
   desc "Symlink shared resources on each release - not used"
